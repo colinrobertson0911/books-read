8bbffc6e4a1430eb0262ebe8890ef59c23760542
Colin Robertson <colinrobertson0911@hotmail.co.uk>
2020-10-21 14:31:58 +0100

    tried to fix findByAuthor not fixed yet for adding book

--- src/main/java/com/example/booksread/service/BookService.java
+++ src/main/java/com/example/booksread/service/BookService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.booksread.model.Author;
 import com.example.booksread.model.Book;
 import com.example.booksread.repository.BookDao;
 
